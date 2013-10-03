799a28c57aaf4c4a13a42a9972653ab83c299146
Richmond Adjei <Richmond.Adjei@gmail.com>
2013-10-03 17:36:32 +0100

    Create a ralis application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treehouse::Application.config.secret_token = 'd87c192b1ee383a0ea0d13727a8572dfd4ea976468f07c6643dfc87209691daaf5538ffcb18dba7327593155bf70bb23f1bf9b55ca8b69380f93b7fa8da5b3c9'
